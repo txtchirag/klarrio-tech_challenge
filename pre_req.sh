77e742523a672480faa31b94fd4cb963f984a351
Chirag <txtchirag@yahoo.com>
2021-05-16 12:52:52 +1000

    Update pre_req.sh

--- pre_req.sh
+++ pre_req.sh
@@ -2,6 +2,6 @@
 
 # install dependencies
 sudo apt-get update
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release net-tools python3-pip
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release net-tools python3-pip awscli
 pip install boto3 paho-mqtt
 
