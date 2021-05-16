e258084787aa70e5a80e44bf9c0611bea1a2d56c
Chirag <txtchirag@yahoo.com>
2021-05-16 12:55:07 +1000

    Update pre_req.sh

--- pre_req.sh
+++ pre_req.sh
@@ -3,5 +3,6 @@
 # install dependencies
 sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release net-tools python3-pip awscli
-pip install boto3 paho-mqtt
+pip install boto3 paho-mqtt 
+pip install --upgrade awscli
 
